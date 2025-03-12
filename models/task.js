2ab48788b9feacbe7ea8e740cc64d02bf213c0c3
teressaborra <teressa_csd@srkrec.edu.in>
2025-03-12 18:48:00 +0530

    now

--- models/task.js
+++ models/task.js
@@ -1,9 +1,12 @@
+
+
+
 const mongoose = require('mongoose');
 
 const taskSchema = new mongoose.Schema({
-    name: { type: String, required: true },
-    age: { type: Number, required: true },
-    branch: { type: String, required: true }
+    title: { type: String, required: true },
+    description: { type: String },
+    status: { type: String, enum: ['pending', 'completed'], default: 'pending' },
 }, { timestamps: true });
 
 module.exports = mongoose.model('Task', taskSchema);
